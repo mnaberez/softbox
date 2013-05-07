5e663096ea2a54aa63a1c4457bcc07aeef6a700d
Mike Naberezny <mike@naberezny.com>
2013-05-06 18:15:48 -0700

    Fix insert line to work on 24 or 25 line screen

--- cbm/cbm2.asm
+++ cbm/cbm2.asm
@@ -1240,13 +1240,19 @@ ctrl_11:
 ;The line at the current position will be erased (filled with spaces).
 ;The current cursor position will not be changed.
 ;
-    lda #<screen+$0780  ;Start address of last 80 col line
+    lda #<screen+$0780  ;Start address of last line on 80x25 screen
     ldy #>screen+$0780
     bit columns         ;80 columns?
     bvs l_0949          ;  Yes: branch to keep address for 80 col
-    lda #<screen+$03c0  ;Start address of last 40 col line
+    lda #<screen+$03c0  ;Start address of last line on 40x25 line screen
     ldy #>screen+$03c0
 l_0949:
+    ldx lines
+    cpx #25             ;25 line screen?
+    beq l_094a          ;  Yes: keep address of last line
+    sec                 ;   No: assume 24 line screen and adjust the address
+    sbc columns
+l_094a:
     sta source_lo
     sty source_hi
     lda #$00
