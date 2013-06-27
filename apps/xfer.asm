51f3d428bd2924e75763d344ebd1a823f2611d8c
Mike Naberezny <mike@naberezny.com>
2013-06-26 17:38:31 -0700

    Remove disassembler output that is no longer needed

--- apps/xfer.asm
+++ apps/xfer.asm
@@ -76,207 +76,207 @@ l011dh:
 seq_from_pet:
 ;Copy sequential file from PET DOS
 ;
-    ld a,53h            ;0152 3e 53
-    call sub_0247h      ;0154 cd 47 02
-    ld hl,0080h         ;0157 21 80 00
+    ld a,53h
+    call sub_0247h
+    ld hl,0080h
 l015ah:
-    call sub_03a6h      ;015a cd a6 03
-    ld d,a              ;015d 57
-    ld a,(eoisav)       ;015e 3a 6c ea
-    and 10h             ;0161 e6 10
-    push af             ;0163 f5
-    ld a,(07e2h)        ;0164 3a e2 07
-    or a                ;0167 b7
-    ld a,d              ;0168 7a
-    jr z,l0174h         ;0169 28 09
-    cp cr               ;016b fe 0d
-    jr nz,l0174h        ;016d 20 05
-    call sub_0234h      ;016f cd 34 02
-    ld a,lf             ;0172 3e 0a
+    call sub_03a6h
+    ld d,a
+    ld a,(eoisav)
+    and 10h
+    push af
+    ld a,(07e2h)
+    or a
+    ld a,d
+    jr z,l0174h
+    cp cr
+    jr nz,l0174h
+    call sub_0234h
+    ld a,lf
 l0174h:
-    call sub_0234h      ;0174 cd 34 02
-    pop af              ;0177 f1
-    jr z,l015ah         ;0178 28 e0
+    call sub_0234h
+    pop af
+    jr z,l015ah
 l017ah:
-    ld de,(table_1+1)   ;017a ed 5b e0 07
-    call ieee_close     ;017e cd 60 f0
-    ld b,7fh            ;0181 06 7f
+    ld de,(table_1+1)
+    call ieee_close
+    ld b,7fh
 l0183h:
-    push bc             ;0183 c5
-    ld a,1ah            ;0184 3e 1a
-    call sub_0234h      ;0186 cd 34 02
-    pop bc              ;0189 c1
-    djnz l0183h         ;018a 10 f7
-    ld de,fcb           ;018c 11 5c 00
-    ld c,fclose         ;018f 0e 10
-    call bdos           ;0191 cd 05 00
-    ld de,complete      ;0194 11 dd 05
-    ld c,cwritestr      ;0197 0e 09
-    call bdos           ;0199 cd 05 00
-    jp warm             ;019c c3 00 00
+    push bc
+    ld a,1ah
+    call sub_0234h
+    pop bc
+    djnz l0183h
+    ld de,fcb
+    ld c,fclose
+    call bdos
+    ld de,complete
+    ld c,cwritestr
+    call bdos
+    jp warm
 
 bas_from_pet:
 ;Copy BASIC program from PET DOS
 ;
-    ld a,50h            ;019f 3e 50
-    call sub_0247h      ;01a1 cd 47 02
-    ld hl,0080h         ;01a4 21 80 00
-    call sub_03a6h      ;01a7 cd a6 03
-    call sub_03a6h      ;01aa cd a6 03
+    ld a,50h
+    call sub_0247h
+    ld hl,0080h
+    call sub_03a6h
+    call sub_03a6h
 l01adh:
-    call sub_03a6h      ;01ad cd a6 03
-    push af             ;01b0 f5
-    call sub_03a6h      ;01b1 cd a6 03
-    pop bc              ;01b4 c1
-    or b                ;01b5 b0
-    jr z,l017ah         ;01b6 28 c2
-    push hl             ;01b8 e5
-    call sub_03a6h      ;01b9 cd a6 03
-    push af             ;01bc f5
-    call sub_03a6h      ;01bd cd a6 03
-    ld d,a              ;01c0 57
-    pop af              ;01c1 f1
-    ld e,a              ;01c2 5f
-    ld bc,2710h         ;01c3 01 10 27
-    call sub_0223h      ;01c6 cd 23 02
-    ld bc,03e8h         ;01c9 01 e8 03
-    call sub_0223h      ;01cc cd 23 02
-    ld bc,0064h         ;01cf 01 64 00
-    call sub_0223h      ;01d2 cd 23 02
-    ld bc,000ah         ;01d5 01 0a 00
-    call sub_0223h      ;01d8 cd 23 02
-    ld a,e              ;01db 7b
-    add a,30h           ;01dc c6 30
-    call sub_0234h      ;01de cd 34 02
-    ld a,20h            ;01e1 3e 20
-    call sub_0234h      ;01e3 cd 34 02
+    call sub_03a6h
+    push af
+    call sub_03a6h
+    pop bc
+    or b
+    jr z,l017ah
+    push hl
+    call sub_03a6h
+    push af
+    call sub_03a6h
+    ld d,a
+    pop af
+    ld e,a
+    ld bc,2710h
+    call sub_0223h
+    ld bc,03e8h
+    call sub_0223h
+    ld bc,0064h
+    call sub_0223h
+    ld bc,000ah
+    call sub_0223h
+    ld a,e
+    add a,30h
+    call sub_0234h
+    ld a,20h
+    call sub_0234h
 l01e6h:
-    call sub_03a6h      ;01e6 cd a6 03
-    or a                ;01e9 b7
-    jr z,l01f4h         ;01ea 28 08
-    jp m,l0200h         ;01ec fa 00 02
-    call sub_0234h      ;01ef cd 34 02
-    jr l01e6h           ;01f2 18 f2
+    call sub_03a6h
+    or a
+    jr z,l01f4h
+    jp m,l0200h
+    call sub_0234h
+    jr l01e6h
 l01f4h:
-    ld a,cr             ;01f4 3e 0d
-    call sub_0234h      ;01f6 cd 34 02
-    ld a,lf             ;01f9 3e 0a
-    call sub_0234h      ;01fb cd 34 02
-    jr l01adh           ;01fe 18 ad
+    ld a,cr
+    call sub_0234h
+    ld a,lf
+    call sub_0234h
+    jr l01adh
 l0200h:
-    ld de,basic4_cmds   ;0200 11 02 06
-    and 7fh             ;0203 e6 7f
-    ld b,a              ;0205 47
-    inc b               ;0206 04
+    ld de,basic4_cmds
+    and 7fh
+    ld b,a
+    inc b
 l0207h:
-    djnz l0218h         ;0207 10 0f
+    djnz l0218h
 l0209h:
-    ld a,(de)           ;0209 1a
-    and 7fh             ;020a e6 7f
-    push de             ;020c d5
-    call sub_0234h      ;020d cd 34 02
-    pop de              ;0210 d1
-    ld a,(de)           ;0211 1a
-    rla                 ;0212 17
-    inc de              ;0213 13
-    jr nc,l0209h        ;0214 30 f3
-    jr l01e6h           ;0216 18 ce
+    ld a,(de)
+    and 7fh
+    push de
+    call sub_0234h
+    pop de
+    ld a,(de)
+    rla
+    inc de
+    jr nc,l0209h
+    jr l01e6h
 l0218h:
-    ld a,(de)           ;0218 1a
-    inc de              ;0219 13
-    cp 0ffh             ;021a fe ff
-    jr z,l01e6h         ;021c 28 c8
-    rla                 ;021e 17
-    jr nc,l0218h        ;021f 30 f7
-    jr l0207h           ;0221 18 e4
+    ld a,(de)
+    inc de
+    cp 0ffh
+    jr z,l01e6h
+    rla
+    jr nc,l0218h
+    jr l0207h
 sub_0223h:
-    push hl             ;0223 e5
-    ex de,hl            ;0224 eb
-    ld a,2fh            ;0225 3e 2f
+    push hl
+    ex de,hl
+    ld a,2fh
 l0227h:
-    inc a               ;0227 3c
-    or a                ;0228 b7
-    sbc hl,bc           ;0229 ed 42
-    jr nc,l0227h        ;022b 30 fa
-    add hl,bc           ;022d 09
-    ex (sp),hl          ;022e e3
-    call sub_0234h      ;022f cd 34 02
-    pop de              ;0232 d1
-    ret                 ;0233 c9
+    inc a
+    or a
+    sbc hl,bc
+    jr nc,l0227h
+    add hl,bc
+    ex (sp),hl
+    call sub_0234h
+    pop de
+    ret
 sub_0234h:
-    ld (hl),a           ;0234 77
-    inc l               ;0235 2c
-    ret nz              ;0236 c0
-    ld de,fcb           ;0237 11 5c 00
-    ld c,fwrite         ;023a 0e 15
-    call bdos           ;023c cd 05 00
-    or a                ;023f b7
-    jp nz,exit_full     ;0240 c2 d4 02
-    ld hl,0080h         ;0243 21 80 00
-    ret                 ;0246 c9
+    ld (hl),a
+    inc l
+    ret nz
+    ld de,fcb
+    ld c,fwrite
+    call bdos
+    or a
+    jp nz,exit_full
+    ld hl,0080h
+    ret
 sub_0247h:
-    push af             ;0247 f5
+    push af
 l0248h:
-    ld de,src_drive     ;0248 11 13 05
-    ld c,cwritestr      ;024b 0e 09
-    call bdos           ;024d cd 05 00
-    call sub_03b8h      ;0250 cd b8 03
-    ld a,(table_0+2)    ;0253 3a 5f 07
-    sub 41h             ;0256 d6 41
-    ld (table_1),a      ;0258 32 df 07
-    call get_dtype      ;025b cd 51 f0
-    jr c,l026ah         ;025e 38 0a
-    ld de,bad_drive     ;0260 11 92 05
-    ld c,cwritestr      ;0263 0e 09
-    call bdos           ;0265 cd 05 00
-    jr l0248h           ;0268 18 de
+    ld de,src_drive
+    ld c,cwritestr
+    call bdos
+    call sub_03b8h
+    ld a,(table_0+2)
+    sub 41h
+    ld (table_1),a
+    call get_dtype
+    jr c,l026ah
+    ld de,bad_drive
+    ld c,cwritestr
+    call bdos
+    jr l0248h
 l026ah:
-    ld de,src_filename  ;026a 11 31 05
-    ld c,cwritestr      ;026d 0e 09
-    call bdos           ;026f cd 05 00
-    call sub_03b8h      ;0272 cd b8 03
-    ld a,(table_1)      ;0275 3a df 07
-    call ieee_init_drv  ;0278 cd 78 f0
-    ld a,(table_1)      ;027b 3a df 07
-    and 01h             ;027e e6 01
-    add a,30h           ;0280 c6 30
-    ld (table_0),a      ;0282 32 5d 07
-    ld a,(table_1)      ;0285 3a df 07
-    call get_ddev       ;0288 cd 54 f0
-    ld hl,table_0+1     ;028b 21 5e 07
-    ld a,(hl)           ;028e 7e
-    add a,06h           ;028f c6 06
-    ld c,a              ;0291 4f
-    ld (hl),3ah         ;0292 36 3a
-    ld hl,0759h         ;0294 21 59 07
-    ld b,00h            ;0297 06 00
-    add hl,bc           ;0299 09
-    ld (hl),2ch         ;029a 36 2c
-    inc hl              ;029c 23
-    pop af              ;029d f1
-    ld (hl),a           ;029e 77
-    inc hl              ;029f 23
-    ld (hl),2ch         ;02a0 36 2c
-    inc hl              ;02a2 23
-    ld (hl),52h         ;02a3 36 52
-    ld hl,table_0       ;02a5 21 5d 07
-    ld e,03h            ;02a8 1e 03
-    ld (table_1+1),de   ;02aa ed 53 e0 07
-    call ieee_open      ;02ae cd 5d f0
-    ld a,(table_1)      ;02b1 3a df 07
-    call ieee_read_err  ;02b4 cd 5a f0
-    or a                ;02b7 b7
-    jp nz,exit_dos_err  ;02b8 c2 ec 03
-    ld de,fcb           ;02bb 11 5c 00
-    ld c,fdelete        ;02be 0e 13
-    call bdos           ;02c0 cd 05 00
-    ld de,fcb           ;02c3 11 5c 00
-    ld c,fmake          ;02c6 0e 16
-    call bdos           ;02c8 cd 05 00
-    inc a               ;02cb 3c
-    ret nz              ;02cc c0
-    ld de,(table_1+1)   ;02cd ed 5b e0 07
-    call ieee_close     ;02d1 cd 60 f0
+    ld de,src_filename
+    ld c,cwritestr
+    call bdos
+    call sub_03b8h
+    ld a,(table_1)
+    call ieee_init_drv
+    ld a,(table_1)
+    and 01h
+    add a,30h
+    ld (table_0),a
+    ld a,(table_1)
+    call get_ddev
+    ld hl,table_0+1
+    ld a,(hl)
+    add a,06h
+    ld c,a
+    ld (hl),3ah
+    ld hl,0759h
+    ld b,00h
+    add hl,bc
+    ld (hl),2ch
+    inc hl
+    pop af
+    ld (hl),a
+    inc hl
+    ld (hl),2ch
+    inc hl
+    ld (hl),52h
+    ld hl,table_0
+    ld e,03h
+    ld (table_1+1),de
+    call ieee_open
+    ld a,(table_1)
+    call ieee_read_err
+    or a
+    jp nz,exit_dos_err
+    ld de,fcb
+    ld c,fdelete
+    call bdos
+    ld de,fcb
+    ld c,fmake
+    call bdos
+    inc a
+    ret nz
+    ld de,(table_1+1)
+    call ieee_close
 
 exit_full:
 ;Print disk full error and return to CP/M
@@ -289,86 +289,86 @@ exit_full:
 seq_to_pet:
 ;Copy sequential file to PET DOS
 ;
-    ld de,dest_drive    ;02df 11 48 05
-    ld c,cwritestr      ;02e2 0e 09
-    call bdos           ;02e4 cd 05 00
-    call sub_03b8h      ;02e7 cd b8 03
-    ld a,(table_0+2)    ;02ea 3a 5f 07
-    sub 41h             ;02ed d6 41
-    ld (table_1),a      ;02ef 32 df 07
-    call get_dtype      ;02f2 cd 51 f0
-    jr c,l0301h         ;02f5 38 0a
-    ld de,bad_drive     ;02f7 11 92 05
-    ld c,cwritestr      ;02fa 0e 09
-    call bdos           ;02fc cd 05 00
-    jr seq_to_pet       ;02ff 18 de
+    ld de,dest_drive
+    ld c,cwritestr
+    call bdos
+    call sub_03b8h
+    ld a,(table_0+2)
+    sub 41h
+    ld (table_1),a
+    call get_dtype
+    jr c,l0301h
+    ld de,bad_drive
+    ld c,cwritestr
+    call bdos
+    jr seq_to_pet
 l0301h:
-    ld de,dest_filename ;0301 11 6b 05
-    ld c,cwritestr      ;0304 0e 09
-    call bdos           ;0306 cd 05 00
-    call sub_03b8h      ;0309 cd b8 03
-    ld a,(table_1)      ;030c 3a df 07
-    call ieee_init_drv  ;030f cd 78 f0
-    ld a,(table_1)      ;0312 3a df 07
-    and 01h             ;0315 e6 01
-    add a,30h           ;0317 c6 30
-    ld (table_0),a      ;0319 32 5d 07
-    ld a,(table_1)      ;031c 3a df 07
-    call get_ddev       ;031f cd 54 f0
-    ld hl,table_0+1     ;0322 21 5e 07
-    ld c,(hl)           ;0325 4e
-    ld b,00h            ;0326 06 00
-    ld ix,table_0+2     ;0328 dd 21 5f 07
-    add ix,bc           ;032c dd 09
-    ld (ix+00h),02ch    ;032e dd 36 00 2c
-    ld (ix+01h),053h    ;0332 dd 36 01 53
-    ld (ix+02h),02ch    ;0336 dd 36 02 2c
-    ld (ix+03h),057h    ;033a dd 36 03 57
-    inc bc              ;033e 03
-    inc bc              ;033f 03
-    inc bc              ;0340 03
-    inc bc              ;0341 03
-    inc bc              ;0342 03
-    inc bc              ;0343 03
-    ld (hl),3ah         ;0344 36 3a
-    dec hl              ;0346 2b
-    ld e,03h            ;0347 1e 03
-    ld (table_1+1),de   ;0349 ed 53 e0 07
-    call ieee_open      ;034d cd 5d f0
-    ld a,(table_1)      ;0350 3a df 07
-    call ieee_read_err  ;0353 cd 5a f0
-    or a                ;0356 b7
-    jp nz,exit_dos_err  ;0357 c2 ec 03
-    ld de,fcb           ;035a 11 5c 00
-    ld c,fopen          ;035d 0e 0f
-    call bdos           ;035f cd 05 00
-    inc a               ;0362 3c
-    jp z,exit_no_file   ;0363 ca 9b 03
+    ld de,dest_filename
+    ld c,cwritestr
+    call bdos
+    call sub_03b8h
+    ld a,(table_1)
+    call ieee_init_drv
+    ld a,(table_1)
+    and 01h
+    add a,30h
+    ld (table_0),a
+    ld a,(table_1)
+    call get_ddev
+    ld hl,table_0+1
+    ld c,(hl)
+    ld b,00h
+    ld ix,table_0+2
+    add ix,bc
+    ld (ix+00h),02ch
+    ld (ix+01h),053h
+    ld (ix+02h),02ch
+    ld (ix+03h),057h
+    inc bc
+    inc bc
+    inc bc
+    inc bc
+    inc bc
+    inc bc
+    ld (hl),3ah
+    dec hl
+    ld e,03h
+    ld (table_1+1),de
+    call ieee_open
+    ld a,(table_1)
+    call ieee_read_err
+    or a
+    jp nz,exit_dos_err
+    ld de,fcb
+    ld c,fopen
+    call bdos
+    inc a
+    jp z,exit_no_file
 l0366h:
-    ld de,fcb           ;0366 11 5c 00
-    ld c,fread          ;0369 0e 14
-    call bdos           ;036b cd 05 00
-    or a                ;036e b7
-    jr nz,l0389h        ;036f 20 18
-    ld de,(table_1+1)   ;0371 ed 5b e0 07
-    call ieee_listen    ;0375 cd 33 f0
-    ld hl,0080h         ;0378 21 80 00
+    ld de,fcb
+    ld c,fread
+    call bdos
+    or a
+    jr nz,l0389h
+    ld de,(table_1+1)
+    call ieee_listen
+    ld hl,0080h
 l037bh:
-    ld a,(hl)           ;037b 7e
-    push hl             ;037c e5
-    call ieee_put_byte  ;037d cd 42 f0
-    pop hl              ;0380 e1
-    inc l               ;0381 2c
-    jr nz,l037bh        ;0382 20 f7
-    call ieee_unlisten  ;0384 cd 36 f0
-    jr l0366h           ;0387 18 dd
+    ld a,(hl)
+    push hl
+    call ieee_put_byte
+    pop hl
+    inc l
+    jr nz,l037bh
+    call ieee_unlisten
+    jr l0366h
 l0389h:
-    ld de,(table_1+1)   ;0389 ed 5b e0 07
-    call ieee_close     ;038d cd 60 f0
-    ld de,complete      ;0390 11 dd 05
-    ld c,cwritestr      ;0393 0e 09
-    call bdos           ;0395 cd 05 00
-    jp warm             ;0398 c3 00 00
+    ld de,(table_1+1)
+    call ieee_close
+    ld de,complete
+    ld c,cwritestr
+    call bdos
+    jp warm
 
 exit_no_file:
 ;Print file not found message and return to CP/M
@@ -379,40 +379,40 @@ exit_no_file:
     jp warm
 
 sub_03a6h:
-    push hl             ;03a6 e5
-    ld de,(table_1+1)   ;03a7 ed 5b e0 07
-    call ieee_talk      ;03ab cd 39 f0
-    call ieee_get_byte  ;03ae cd 3f f0
-    push af             ;03b1 f5
-    call ieee_untalk    ;03b2 cd 3c f0
-    pop af              ;03b5 f1
-    pop hl              ;03b6 e1
-    ret                 ;03b7 c9
+    push hl
+    ld de,(table_1+1)
+    call ieee_talk
+    call ieee_get_byte
+    push af
+    call ieee_untalk
+    pop af
+    pop hl
+    ret
 sub_03b8h:
-    ld de,table_0       ;03b8 11 5d 07
-    ld a,50h            ;03bb 3e 50
-    ld (de),a           ;03bd 12
-    ld c,creadstr       ;03be 0e 0a
-    call bdos           ;03c0 cd 05 00
-    call newline        ;03c3 cd 08 04
-    ld a,(table_0+1)    ;03c6 3a 5e 07
-    ld hl,table_0+2     ;03c9 21 5f 07
-    ld b,a              ;03cc 47
-    inc b               ;03cd 04
+    ld de,table_0
+    ld a,50h
+    ld (de),a
+    ld c,creadstr
+    call bdos
+    call newline
+    ld a,(table_0+1)
+    ld hl,table_0+2
+    ld b,a
+    inc b
 l03ceh:
-    ld a,(hl)           ;03ce 7e
-    cp 61h              ;03cf fe 61
-    jr c,l03dah         ;03d1 38 07
-    cp 7bh              ;03d3 fe 7b
-    jr nc,l03dah        ;03d5 30 03
-    sub 20h             ;03d7 d6 20
-    ld (hl),a           ;03d9 77
+    ld a,(hl)
+    cp 61h
+    jr c,l03dah
+    cp 7bh
+    jr nc,l03dah
+    sub 20h
+    ld (hl),a
 l03dah:
-    inc hl              ;03da 23
-    djnz l03ceh         ;03db 10 f1
-    dec hl              ;03dd 2b
-    ld (hl),cr          ;03de 36 0d
-    ret                 ;03e0 c9
+    inc hl
+    djnz l03ceh
+    dec hl
+    ld (hl),cr
+    ret
 
 exit_bad_file:
 ;Print bad filename message and return to CP/M.
