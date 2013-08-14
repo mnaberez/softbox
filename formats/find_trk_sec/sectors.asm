27b8f7766a04df0887b89ede6a685f7b2ea4f852
Mike Naberezny <mike@naberezny.com>
2013-08-14 10:57:36 -0700

    Use BIOS entry points to set CP/M track/sector

--- formats/find_trk_sec/sectors.asm
+++ formats/find_trk_sec/sectors.asm
@@ -2,38 +2,16 @@
 ;  CP/M track, CP/M sector, PET track, PET sector
 ;
 ;Must be run against the 1983-06-09 BIOS.
-;
-;CBM 8050:
-;
-;  3984: 128 Byte Record Capacity
-;   498: Kilobyte Drive  Capacity
-;    64: 32  Byte Directory Entries
-;    64: Checked  Directory Entries
-;   256: Records/ Extent
-;    16: Records/ Block
-;    32: Sectors/ Track
-;     0: Reserved Tracks
-;
-;CBM 4040:
-;
-;  1232: 128 Byte Record Capacity
-;   154: Kilobyte Drive  Capacity
-;    64: 32  Byte Directory Entries
-;    64: Checked  Directory Entries
-;   256: Records/ Extent
-;    16: Records/ Block
-;    32: Sectors/ Track
-;     0: Reserved Tracks
-;
 
-x_drive:        equ 0044h   ;CP/M drive number
-x_track:        equ 0045h   ;CP/M track
-x_sector:       equ 0047h   ;CP/M sector
 dos_trk:        equ 004dh   ;CBM track
 dos_sec:        equ 004eh   ;CBM sector
 
-conout:         equ 0f00ch  ;char in C
-put_hex_byte:   equ 0f402h  ;byte in A
+conout:         equ 0f00ch  ;Print char in C
+seldsk:         equ 0f01bh  ;Select disk drive
+settrk:         equ 0f01eh  ;Set track number
+setsec:         equ 0f021h  ;Set sector number
+put_hex_byte:   equ 0f402h  ;Print byte in A as hex
+sub_f6b9h:      equ 0f6b9h
 find_trk_sec:   equ 0f8dah
 
 lf:             equ 0ah     ;Line Feed
@@ -42,27 +20,41 @@ cr:             equ 0dh     ;Carriage Return
     org 0100h   ;CP/M TPA
 
 init:
-    ld a,2              ;drive 2 (c:)
-    ld (x_drive),a
+    ld de,0             ;log disk in as new
+    ld c,2              ;drive 2 (c:)
+    call seldsk         ;select disk
 
     xor a
-    ld (x_track),a
-    ld (x_sector),a
+    ld (cur_track),a    ;zero the current cp/m track
+    ld (cur_sector),a   ;zero the current cp/m sector
 
 loop:
+    ;set cp/m track and sector
+
+    ld b,0
+    ld a,(cur_track)
+    ld c,a
+    call settrk         ;set track
+
+    ld b,0
+    ld a,(cur_sector)
+    ld c,a
+    call setsec         ;set sector
+
     ;print cp/m track and sector
 
-    ld a,(x_track)      ;track
+    ld a,(cur_track)    ;track
     call put_hex_byte   ;print track
     ld c,','            ;comma character
     call conout         ;print comma
-    ld a,(x_sector)     ;sector
+    ld a,(cur_sector)   ;sector
     call put_hex_byte   ;print sector
     ld c,','            ;comma character
     call conout         ;print comma
 
     ;compute dos_trk and dos_sec
 
+    call sub_f6b9h
     call find_trk_sec
 
     ;print cbm track and sector
@@ -83,20 +75,26 @@ loop:
 
     ;increment sector
 
-    ld a,(x_sector)     ;sector
+    ld a,(cur_sector)   ;sector
     inc a               ;increment to next sector
-    ld (x_sector),a     ;save it
+    ld (cur_sector),a   ;save it
     cp 20h              ;past last sector on track?
     jp nz,loop          ;  no: loop to do next sector
 
     ;increment track
 
     xor a
-    ld (x_sector),a     ;zero sector
-    ld a,(x_track)      ;track
+    ld (cur_sector),a   ;zero sector
+    ld a,(cur_track)    ;track
     inc a               ;increment to next track
-    ld (x_track),a      ;save it
+    ld (cur_track),a    ;save it
     cp 7eh              ;past last track?
     jp nz,loop          ;  no: loop to do next track
 
     ret                 ;all done
+
+cur_track:
+    db 0
+
+cur_sector:
+    db 0
